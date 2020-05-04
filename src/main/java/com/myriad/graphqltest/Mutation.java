10e7c2139b1e9e86365c24a1e4f93a541ce7f572
ntirpankar <tirpankar.n@gmail.com>
2020-05-04 15:54:02 -0700

    Add in the Mutation

--- src/main/java/com/myriad/graphqltest/Mutation.java
+++ src/main/java/com/myriad/graphqltest/Mutation.java
@@ -3,10 +3,12 @@ package com.myriad.graphqltest;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.myriad.graphqltest.dao.PostDao;
 import com.myriad.graphqltest.models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Mutation implements GraphQLMutationResolver {
+        @Autowired
         private PostDao postDao;
 
         public Post writePost(String title, String text, String category) {
